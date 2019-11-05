801e8fe518c86622aa582d1e5d78e7fbbfeabdad
rumesh <rumesh.m.s16@gmail.com>
2019-11-05 22:47:05 +0530

    customer route done

--- routes/customer/index.js
+++ routes/customer/index.js
@@ -2,28 +2,30 @@ const fs = require("fs");
 const router = require("express").Router();
 const connection = require("../../db");
 const path = require('path');
-let json_response = JSON.parse(fs.readFileSync(path.resolve(__dirname,"../../response_format.json"),'utf8'));
+let json_response = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../response_format.json"), 'utf8'));
 
 router.get('/:id', (req, res) => {
     let id = req.params['id'];
     let data = {};
-    connection.query("select id, first_name, last_name, email, contact_no from customer where id = ?",id,
-        (error,results,fields)=>{
-            if(error) {
-                console.log("error: ", error);
-            }
-            else{
+    connection.query("select id, first_name, last_name, email, contact_no from customer where id = ?", id,
+        (error, results, fields) => {
+            if (error) {
+                console.error("error: ", error);
+                json_response['success'] = false;
+                json_response['message'] = error;
+                res.json(json_response);
+            } else {
                 //console.log(results);
-                results=results[0];
+                results = results[0];
                 data['id'] = results['id'];
                 data['first_name'] = results['first_name'];
                 data['last_name'] = results['last_name'];
                 data['email'] = results['email'];
                 data['contact_no'] = results['contact_no'];
-
+                json_response['data'].push(data);
                 res.json(json_response);
             }
-    });
+        });
 
 
 });
@@ -34,6 +36,62 @@ router.get('/:id', (req, res) => {
 //     connection.query("select id, password from customer where id = ?",id)
 // });
 
+router.post('/', (req, res) => {
+    let request_body = req.body;
+    console.log(request_body);
+    connection.query("insert into customer(first_name, last_name, email, contact_no, password) values (?,?,?,?,?)",
+        [request_body['first_name'], request_body['last_name'], request_body['email'],
+            request_body['contact_no'],
+            request_body['password']], (error, results, fields) => {
+            if (error) {
+                console.error("error: ", error);
+                json_response['success'] = false;
+                json_response['message'] = error;
+                res.json(json_response);
+            } else {
+                let affected_rows = results.affectedRows;
+                json_response['message'] = 'Affected Rows: ' + affected_rows;
+                res.json(json_response);
+            }
+        });
 
+});
+
+router.put('/:id', (req, res) => {
+    let request_body = req.body;
+    let id = req.params['id'];
+    console.log(request_body);
+    connection.query("update customer set first_name=?, last_name=?, email=?, contact_no=? ,password=? where id=?",
+        [request_body['first_name'], request_body['last_name'], request_body['email'],
+            request_body['contact_no'],
+            request_body['password'], id], (error, results, fields) => {
+            if (error) {
+                console.error("error: ", error);
+                json_response['success'] = false;
+                json_response['message'] = error;
+                res.json(json_response);
+            } else {
+                let affected_rows = results.affectedRows;
+                json_response['message'] = 'Affected Rows: ' + affected_rows;
+                res.json(json_response);
+            }
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    let id = req.params['id'];
+    connection.query("delete from customer where id=?", [id], (error, results, fields) => {
+        if (error) {
+            console.error("error: ", error);
+            json_response['success'] = false;
+            json_response['message'] = error;
+            res.json(json_response);
+        } else {
+            let affected_rows = results.affectedRows;
+            json_response['message'] = 'Affected Rows: ' + affected_rows;
+            res.json(json_response);
+        }
+    })
+});
 module.exports = router;
 
