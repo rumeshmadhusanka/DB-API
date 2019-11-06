30f7d6c9b66ce2ba4988701a3a162b7e61b01992
rumesh <rumesh.m.s16@gmail.com>
2019-11-06 17:41:15 +0530

    db conf

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
+const path = require('path');
+const fs = require('fs');
+let db_config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.conf.json"), 'utf8'));
+
 
 //local mysql db connection
-const connection = mysql.createConnection({
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12310953',
-    password: 'Rg4zS5ceei',
-    database: 'sql12310953'
-});
+const connection = mysql.createConnection(db_config);
 
 connection.connect(function (err) {
     if (err) throw err;
