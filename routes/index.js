d3e516c1ba932bac1c2bee1ffd99131369d116b1
rumesh <rumesh.m.s16@gmail.com>
2019-11-06 17:47:05 +0530

    fixed / route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
-    app.use("/", (req, res) => {
-        res.json({"message": "Welcome to the API"})
-    });
     app.use("/customer", require("./customer"));
     app.use("/item", require("./item"));
     app.use("/order", require("./order"));
     app.use("/shop", require("./shop"));
+    app.use("/", (req, res) => {
+        res.json({"message": "Welcome to the API"})
+    });
 };
