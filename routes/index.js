3d571f1359f7684602548595ade48f56b749c1c1
rumesh <rumesh.m.s16@gmail.com>
2019-12-05 22:11:32 +0530

    deleted unnecessary folders

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 module.exports = function (app) {
-    // app.use("/customer", require("./customer"));
-    // app.use("/item", require("./item"));
-    // app.use("/order", require("./order"));
-    // app.use("/shop", require("./shop"));
     app.use("/reg_user", require("./reg_user"));
     //Root route-REMOVE this
     app.use("/", (req, res) => {
-        res.json({"message": "Welcome to the API"})
+        res.status(404).json({"message": "Default Route"})
     });
 };
-
