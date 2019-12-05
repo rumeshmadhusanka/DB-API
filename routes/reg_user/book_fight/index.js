705911c8685b59685bfc89de5556d0afc6fe883d
KushanChamindu <www.kushanchamindu@gmail.com>
2019-12-06 04:44:09 +0530

    book_fight/get/:id  created

--- routes/reg_user/book_fight/index.js
+++ routes/reg_user/book_fight/index.js
@@ -33,4 +33,33 @@ router.get('/all', (req, res) => {
 
 });
 
+router.get('/:id', (req, res) => {
+    let id = req.params['id'];
+    let query = "SELECT * FROM schedule WHERE schedule_id=?";
+    connection.query(query,id,(error, results,fields) => {
+        let json_response = json_response_model(); //returns a new object
+        if (error) {
+            json_response.success = false;
+            json_response.message = "Internal server error";
+            console.log(error);
+            logger.log(error);
+            res.status(502).json(json_response);
+        } else if (typeof(results.lenght) =="undefined") {
+            json_response.success = false;
+            json_response.message = "No fight found";
+            res.status(404).json(json_response);
+        } else {
+            json_response.success = true;
+            json_response.message = "";
+            json_response.data=results;
+            // console.log(results.lenght);
+            res.status(200).json(json_response);
+        }
+
+
+    })
+
+
+});
+
 module.exports = router;
