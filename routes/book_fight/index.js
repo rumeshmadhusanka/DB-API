868f63238abca09a8a35c2afefc0f4a650a09088
KushanChamindu <www.kushanchamindu@gmail.com>
2019-12-09 21:35:28 +0530

    book fight post resquest finished

--- routes/book_fight/index.js
+++ routes/book_fight/index.js
@@ -1,3 +1,5 @@
+//number of books>5 frequent customer and number of books>10 gold customer 
+
 let json_response_model = require('../../json_response');  //A function that returns the json response format object
 const router = require("express").Router();
 const path = require('path');
@@ -66,11 +68,12 @@ router.get('/:id', (req, res) => {
 
     router.post('/:id',(req,res)=>{ //autentification if register user
         let id = req.params['id'];
-        let customerId = req.userId;
+        // let customer_Id = req.userId;
         let schedule_id=req.body.schedule_id;
         let seat_id=req.body.seat_id;
         let user_id=req.body.user_id;
         let json_response = json_response_model(); //Json response object created
+        console.log(id,schedule_id,seat_id,user_id);
         connection.beginTransaction((error) => {
             if(error){
             json_response['success'] = false;
@@ -78,9 +81,45 @@ router.get('/:id', (req, res) => {
             res.status(501).json(json_response);
             }else{
                 var todaydate = new Date().toISOString().slice(0,10);
-                let query="INSERT INTO `book` (`id`, `date`, `schedule_id`, `seat_id`, `user_id`) VALUES (NULL, ?, ?, ?,?)"
-                connection.query(query,null,todaydate,schedule_id,seat_id,user_id,(error,results)=>{
+                //console.log(todaydate)
+                let query="INSERT INTO `book` (`id`, `date`, `schedule_id`, `seat_id`, `user_id`) VALUES (?,?,?,?,?)";
+                connection.query(query,[null,todaydate,schedule_id,seat_id,user_id],(error,results)=>{
+                    // console.log(todaydate);
+                    // let error_code=200;
                     if(error){
+                        json_response['success'] = false;
+                        json_response['message'] = error;
+                        res.status(501).json(json_response);
+                    }else{
+                        let price_query="SELECT get_Total_price_with_discount("+user_id+","+seat_id+")"
+                        connection.query(price_query,(error,result)=>{
+                            let error_code;
+                            if(error){
+                                error_code=501;
+                            }else{
+                                connection.commit((error) => {
+                                    if (error) {
+                                        connection.rollback();
+                                        json_response['success'] = false;
+                                        json_response['message'] = error;
+                                        res.status(400).json(json_response);
+                                    }else if (error_code==501){
+                                        connection.rollback();
+                                        json_response['success'] = false;
+                                        json_response['message'] = error;
+                                        res.status(error_code).json(json_response);
+                                    }else {
+                                        json_response['success'] = true;
+                                        json_response['message'] = "Successully book seat";
+                                        results["total_price"]=result[0][Object.keys(result[0])[0]];
+                                        json_response["data"]=results;
+                                        // console.log(result[0][Object.keys(result[0])[0]]);
+                                        res.status(200).json(json_response);
+                                    }
+                                
+                                });
+                            }
+                        });
                         
                     }
                 });
