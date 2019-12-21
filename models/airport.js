2b4111c14ad9ff515afaa2d2da051ae4bdd66368
KushanChamindu <www.kushanchamindu@gmail.com>
2019-12-21 18:29:06 +0530

    get airport by id request created

--- models/airport.js
+++ models/airport.js
@@ -16,8 +16,8 @@ Airport.prototype.getallairport= async function(){
             } else {
                 resolve(result);
             }
-        } catch (error) {
-            logger.log(error);
+        } catch (e) {
+            logger.log(e);
             reject(new ErrorHandler(502, "Internal Server Error"));
         }
     });
@@ -25,8 +25,8 @@ Airport.prototype.getallairport= async function(){
 Airport.prototype.addairport=async function(airport_req){
     let query= "INSERT INTO `airport` (`location_id`, `name`, `code`) VALUES (?,?,?);";
     return new Promise(async(resolve,reject)=>{
-        let pool = await poolPromise;
         try {
+            let pool = await poolPromise;
             let result = await pool.query(query,[airport_req.location_id,airport_req.name,airport_req.code]);
             resolve(result);
         } catch (e) {
@@ -37,6 +37,23 @@ Airport.prototype.addairport=async function(airport_req){
                 // console.log(e);
                 reject(new ErrorHandler(502, "Internal Server Error"));
             }
+        }
+    });
+}
+Airport.prototype.getairportbyid=async function(airport_id){
+    let query="select * from airport where airport_id=?";
+    return new Promise(async(resolve,reject)=>{
+        try {
+            let pool = await poolPromise;
+            let result=await pool.query(query,[airport_id]);
+            if (!result.length) {
+                reject(new ErrorHandler(404, "No Airport found"));
+            } else {
+                resolve(result);
+            }
+        } catch (e) {
+            logger.log(e);
+            reject(new ErrorHandler(502, "Internal Server Error"));
         }
     });
 }
