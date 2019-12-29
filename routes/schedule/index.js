794be8032cf59aaf2f920d5d366141e2ff67e1a3
rumesh <rumesh.m.s16@gmail.com>
2019-12-29 17:50:15 +0530

    edited the db conf to support multiple statements Schedule filtering done

--- routes/schedule/index.js
+++ routes/schedule/index.js
@@ -7,10 +7,21 @@ const Schedule = require('../../models/schedule');
 router.get('/', async (req, res) => {
     let airplane_obj = new Schedule();
     let json_response = json_response_model();
+    let origin = req.query.origin;
+    let destination = req.query.destination;
+    let from_date = req.query.from_date;
+    let to_date = req.query.to_date;
     try {
-        json_response.data = await airplane_obj.getAllSchedules();
-        json_response.success = true;
-        res.status(200).json(json_response);
+
+        if (!origin && !destination && !from_date && !to_date) {
+            json_response.data = await airplane_obj.getAllSchedules();
+            json_response.success = true;
+            res.status(200).json(json_response);
+        } else {
+            json_response.data = await airplane_obj.getRestrictedSchedules(origin, destination, from_date, to_date);
+            json_response.success = true;
+            res.status(200).json(json_response);
+        }
     } catch (e) {
         json_response.message = e;
         let code = e.statusCode || 502;
