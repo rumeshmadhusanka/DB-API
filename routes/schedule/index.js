e1d95ce9a43c430cafa5c3ee4e5f6f5cb9afbd97
rumesh <rumesh.m.s16@gmail.com>
2019-12-29 18:10:28 +0530

    POST schedule

--- routes/schedule/index.js
+++ routes/schedule/index.js
@@ -29,4 +29,37 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+router.post('/', async (req, res) => {
+    let airplane_obj = new Schedule();
+    let json_response = json_response_model();
+    let schedule_req = {
+
+        date: req.body.date,
+        flight_id: req.body.flight_id,
+        dep_time: req.body.dep_time,
+        arrival_time: req.body.arrival_time,
+        gate_id: req.body.gate_id
+    };
+    try {
+        // await Joi_schema.airplane_post.validateAsync(airplane_req); todo add validation
+        await airplane_obj.createNewSchedule(schedule_req.date,
+            schedule_req.flight_id, schedule_req.dep_time,
+            schedule_req.arrival_time, schedule_req.gate_id);
+        json_response.success = true;
+        json_response.message = "Schedule successfully added";
+        res.status(201).json(json_response);
+    } catch (e) {
+        json_response.message = e;
+        let code = e.statusCode || 502;
+        if (e._message == null && e.details[0].message) {
+            code = 400;
+            json_response.message = e.details[0].message;
+            res.status(code).json(json_response);
+        } else {
+            res.status(code).json(json_response);
+        }
+    }
+});
+
 module.exports = router;
