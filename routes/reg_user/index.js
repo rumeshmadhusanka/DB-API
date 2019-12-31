11549490809b577f038fed699ca93c436b011a75
rumesh <rumesh.m.s16@gmail.com>
2019-12-31 12:39:29 +0530

    seat.yaml
    login for reg_user done

--- routes/reg_user/index.js
+++ routes/reg_user/index.js
@@ -4,10 +4,13 @@ const path = require('path');
 const connection = require("../../db");
 const logger = require("../../logger");
 const RegUser = require('../../models/RegUser');
-const Joi_schema=require('../../validation/book_fight_schema'); 
+const Joi_schema = require('../../validation/book_fight_schema');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../env.config.json'), 'utf8'));
 
 router.get('/:id', async (req, res) => {
-    let id_params = {id:req.params['id']};
+    let id_params = {id: req.params['id']};
     let user = new RegUser();
     let json_response = json_response_model();
     try {
@@ -50,7 +53,7 @@ router.post('/', async(req, res) => {
         json_response.success = true;
         let send_results = results[0];
         json_response.message = "Successfully added " + first_name;
-        res.status(200).json(json_response);
+        res.status(201).json(json_response);
     } catch (e) {
         json_response.message = e;
         let code = e.statusCode || 502;
@@ -67,12 +70,41 @@ router.post('/', async(req, res) => {
 
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let user = new RegUser();
+    let json_response = json_response_model();
+    try {
 
+        let results = await user.login(email, password);
+        json_response.success = true;
+        let send_results = results[0];
+        let user_id = send_results["user_id"];
+        //console.log(send_results);
+        //console.log();
+        json_response.message = "Login Success";
+        json_response['token'] = jwt.sign({"user_id": user_id}, config.secret, config.options);
+        res.status(200).json(json_response);
+    } catch (e) {
+        console.log(e);
+        json_response.message = e;
+        let code = e.statusCode || 502;
+        if (e._message == null && e.details[0].message) {
+            code = 400;
+            json_response.data =
+                json_response.message = e.details[0].message;
+            res.status(code).json(json_response);
+        } else {
+
+            res.status(code).json(json_response);
+        }
+        res.status(502).send();
+    }
 });
 
-router.put('/:id', (req, res) => {
-    let user_id=req.params["id"];
+router.put('/:id', async (req, res) => {
+    let user_id = req.params["id"];
     let first_name = req.body.first_name;
     let second_name = req.body.second_name;
     let email = req.body.email;
