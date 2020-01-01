7c4631132676b64d1a5022992ac64fe7b3fc7d6d
KushanChamindu <www.kushanchamindu@gmail.com>
2020-01-01 15:26:08 +0530

    minor change

--- models/seat.js
+++ models/seat.js
@@ -24,7 +24,7 @@ Seat.prototype.getseatbyid=async function(schedule_id){
 }
 Seat.prototype.getfreeseatbyid=async function(schedule_id){
     // let query="select * from seat where model_id=?";
-    let query = "SELECT a.seat_id,a.seat_name,a.seat_class,a.seat_price FROM (SELECT seat_id,seat_name,seat_class,seat_price from `seat_details_according_to_schedule` where schedule_id=?)a WHERE seat_id not in (SELECT seat_id from book WHERE schedule_id=?)"
+    let query = "SELECT a.seat_id,a.seat_name,a.class,a.price FROM(SELECT seat_id,seat_name,class,price FROM `seat_details_according_to_schedule` WHERE schedule_id = ?) a WHERE seat_id NOT IN(SELECT seat_id FROM book WHERE schedule_id = ?)"
     return new Promise(async(resolve,reject)=>{
         try {
             let pool = await poolPromise;
